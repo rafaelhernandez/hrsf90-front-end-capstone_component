0d900e3047aa49c2a3e3339df01c2e7b2c14f0da
Rafael Hernandez <rhmnospam@gmail.com>
2018-03-04 01:20:19 -0800

    Implement api service

--- client/src/js/index.jsx
+++
@@ -1,4 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-
-ReactDOM.render(<App roomId="2736358"></App>, document.getElementById('app'));
--- server/server.js
+++
@@ -1,4 +0,0 @@
-const app = require('./app.js');
-
-let port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Express listening on port ', port));
